d3d374855e067a36f0943defe03b9dd15a4530ce
nmirandaghn <nmirandaghn@gmail.com>
2015-10-07 08:56:05 -0600

    Added Boat class and completed Employee class

--- employee2.rb
+++ employee2.rb
@@ -8,6 +8,10 @@ class Employee
     @name = name
   end
 
+  def initialize(name = "Anonymous")
+    self.name = name
+  end
+
   def print_name
     puts "Name: #{name}"
   end
@@ -23,6 +27,11 @@ class SalariedEmployee < Employee
     @salary = salary
   end
 
+  def initialize(name = "Anonymous", salary = 0.0)
+    super(name)
+    self.salary = salary
+  end
+
   def print_pay_stub
     print_name
     pay_for_period = (self.salary / 365.0) * 14
@@ -32,6 +41,18 @@ class SalariedEmployee < Employee
 end
 
 class HourlyEmployee < Employee
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+
+  def self.cashier(name)
+    HourlyEmployee.new(name, 12.75, 25)
+  end
+
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.5, 20)
+  end
+
   attr_reader :hourly_wage, :hours_per_week
 
   def hourly_wage=(value)
@@ -46,6 +67,12 @@ class HourlyEmployee < Employee
       raise "Hours per week can't be negative"
     end
     @hours_per_week = value
+  end
+
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
   end
 
   def print_pay_stub
@@ -68,3 +95,11 @@ h.name = "John Smith"
 h.hourly_wage = 14.97
 h.hours_per_week = 30
 h.print_pay_stub
+
+angela = HourlyEmployee.security_guard("Angela Mathews")
+edwin = HourlyEmployee.janitor("Edwin Burgess")
+ivan = HourlyEmployee.cashier("Ivan Stokes")
+
+angela.print_pay_stub
+edwin.print_pay_stub
+ivan.print_pay_stub
